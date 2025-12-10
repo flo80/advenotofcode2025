999688bfebac98d985c1af98a76ad002afaf4944
Florian Lloyd-Pötscher <florian@luke-roberts.com>
2025-12-10 23:06:09 +0100

    change manual iteration of potential pushed buttons by generating all options

--- day10.zig
+++ day10.zig
@@ -2,63 +2,31 @@ const std = @import("std");
 const print = std.debug.print;
 const Allocator = std.mem.Allocator;
 
-fn parseLight(str: []const u8) u16 {
-    var result: u16 = 0;
+const Button = u16;
+const Light = Button;
+const max_buttons = @bitSizeOf(Button);
+const BitInButton = std.math.Log2Int(Button);
+
+fn parseLight(str: []const u8) Light {
+    var result: Light = 0;
     for (str[1 .. str.len - 1], 0..) |char, i| {
-        if (char == '#') result |= @as(u16, 1) << @as(u4, @intCast(i));
+        if (char == '#') result |= @as(Light, 1) << @as(BitInButton, @intCast(i));
     }
     return result;
 }
 
-fn parseButton(str: []const u8) !u16 {
+fn parseButton(str: []const u8) !Button {
     var nrs = std.mem.splitScalar(u8, str[1 .. str.len - 1], ',');
-    var result: u16 = 0;
+    var result: Button = 0;
     while (nrs.next()) |nr_string| {
-        const nr = try std.fmt.parseInt(u4, nr_string, 10);
-        result |= @as(u16, 1) << nr;
+        const nr = try std.fmt.parseInt(BitInButton, nr_string, 10);
+        result |= @as(Button, 1) << nr;
     }
     return result;
 }
 
-const maxButtons = 16;
-
-fn findCombination(target: u16, buttons: []u16, number_of_buttons: u4) bool {
-    var used_buttons_buffer: [maxButtons]u4 = undefined;
-    var used_buttons = std.ArrayList(u4).initBuffer(&used_buttons_buffer);
-
-    std.debug.assert(number_of_buttons <= buttons.len);
-
-    for (0..number_of_buttons) |button_nr| {
-        used_buttons.appendAssumeCapacity(@as(u4, @truncate(button_nr)));
-    }
-    std.debug.assert(used_buttons.items.len == number_of_buttons);
-
-    check: while (true) {
-        var current_number: u16 = 0;
-        for (used_buttons.items) |button_nr| current_number ^= buttons[button_nr];
-
-        if (current_number == target) return true;
-
-        getNr: while (true) {
-            if (used_buttons.pop()) |last| {
-                if (last < buttons.len - 1) {
-                    used_buttons.appendAssumeCapacity(last + 1);
-
-                    while (used_buttons.items.len < number_of_buttons) {
-                        const next = used_buttons.getLast() + 1;
-                        if (next >= buttons.len) continue :getNr;
-                        used_buttons.appendAssumeCapacity(next);
-                    }
-
-                    continue :check;
-                } // else: go back to popping last number
-            } else {
-                // we ran out of options for the first number
-                return false;
-            }
-        }
-        return false;
-    }
+fn sortByPopCount(_: void, a: Button, b: Button) bool {
+    return @popCount(a) < @popCount(b);
 }
 
 fn partA(allocator: Allocator, input: []const u8) !usize {
@@ -72,8 +40,8 @@ fn partA(allocator: Allocator, input: []const u8) !usize {
         var tokens = std.mem.splitScalar(u8, line, ' ');
         const lights = parseLight(tokens.next().?);
 
-        var buttons_buffer: [maxButtons]u16 = undefined;
-        var buttons = std.ArrayList(u16).initBuffer(&buttons_buffer);
+        var buttons_buffer: [max_buttons]Button = undefined;
+        var buttons = std.ArrayList(Button).initBuffer(&buttons_buffer);
 
         while (tokens.next()) |token| {
             if (token[0] == '{') break;
@@ -81,9 +49,21 @@ fn partA(allocator: Allocator, input: []const u8) !usize {
             buttons.appendAssumeCapacity(button);
         }
 
-        for (1..buttons.items.len + 1) |max_buttons| {
-            if (findCombination(lights, buttons.items, @as(u4, @truncate(max_buttons)))) {
-                part_a += max_buttons;
+        var potential_options_buffer: [(1 << max_buttons)]Button = undefined;
+        var potential_options = std.ArrayList(Button).initBuffer(&potential_options_buffer);
+        for (1..(@as(Button, 1) << @as(BitInButton, @truncate(buttons.items.len)))) |b| potential_options.appendAssumeCapacity(@as(Button, @truncate(b)));
+        std.mem.sortUnstable(Button, potential_options.items, {}, sortByPopCount);
+
+        for (potential_options.items) |used_buttons| {
+            var current_lights: Button = 0;
+            for (buttons.items, 0..) |button_value, i| {
+                const button_number = @as(Button, 1) << @as(BitInButton, @truncate(i));
+                if (button_number & used_buttons > 0)
+                    current_lights ^= button_value;
+            }
+
+            if (current_lights == lights) {
+                part_a += @popCount(used_buttons);
                 continue :check;
             }
         }
